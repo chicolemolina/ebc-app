2ee9f1a3b6bd44b46cd4e26317319c6bd8b80463
unknown <chicolemolina>
2024-03-27 12:20:06 +0100

    07

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com'
+      },
       {
         protocol: 'https',
         hostname: 'socios.excellencebusinessclub.com'
