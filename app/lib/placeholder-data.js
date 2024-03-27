7d56bed5c4a25f3d81726b75e9eba4d64ff65d62
unknown <chicolemolina>
2024-03-27 13:14:43 +0100

    09

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,12 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442b',
+    name: 'User',
+    email: 'prueba@nextmail.com',
+    password: 'admin22',
+  },
 ];
 
 const customers = [
